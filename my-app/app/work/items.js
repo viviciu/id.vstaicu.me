06b6086b710d63bdf103938ff638f0e0513d0e48
viviciu <vstaicu@andrew.cmu.edu>
2025-02-25 18:22:33 -0500

    spatula added

--- my-app/app/work/items.js
+++ my-app/app/work/items.js
@@ -1,36 +1,7 @@
 // A separate file with items (url should contain a link for each image)
 // include the tools you're proficient in in your about page.
 export const items = [
-  {
-    id: "elysium",
-    name: "Lunar Gala: Elysium",
-    url: "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/elysium/d05qjnyo3ahj9quokazr",
-    // url: "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/elysium/peypqarqejx6fezrlawh",
-    // url: "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/elysium/n5xvgcsapxlnsxyfuzsp",
-    fileType: "mp4",
-    category: "UI/UX, Creative Code, Brand",
-    categorySpecific: "",
-  },
-
   // YOU MUST KEEP F_AUTO:VIDEO, NOT F_MP4, IN ORDER TO USE IT WITH AN <Image /> tag
-  {
-    id: "touchdesignerSeries",
-    name: "Touchdesigner Experiments",
-    url: "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/touchdesigner/tn1qnhqpbextyksfdpvk",
-    // OLD GIF: "https://res.cloudinary.com/ddjxpkewq/image/upload/f_auto,q_auto/v1/touchdesigner/e8cbelbypsufwsk0nsly"
-    fileType: "mp4",
-    category: "Generative Design",
-    categorySpecific: "some generative procedural animations.",
-  },
-
-  {
-    id: "typographyAnimation",
-    name: "Kinetic type animation",
-    url: "https://res.cloudinary.com/ddjxpkewq/image/upload/f_auto,q_auto/v1/c-animation/s3k9v38udigxjapryyxa",
-    fileType: "img",
-    category: "Motion",
-    categorySpecific: "a study in time and motion narrating a typeface.",
-  },
   {
     id: "playfulVsPractical",
     name: "Playful vs. Practical",
@@ -41,36 +12,30 @@ export const items = [
     categorySpecific: "a synergy of language, form, and context.",
   },
   {
-    id: "liminal",
-    name: "Lunar Gala: Liminal",
-    url: "/imgs/lg-home-COVER.webp",
+    id: "spatula",
+    name: "Spatula",
+    url: "https://res.cloudinary.com/ddjxpkewq/image/upload/f_auto,q_auto/v1/play/lohb80s9ngarowqrs78s",
+    // url: "/imgs/DSC05668-thumbnail.jpg",
     fileType: "img",
-    category: "UI/UX, Creative Code, Brand",
-    categorySpecific:
-      "a realm where the very essence of self and time finds itself in flux.",
+    category: "Industrial Design",
+    categorySpecific: "",
   },
-
+  
   {
-    id: "cminiPoster",
-    name: "Dirty Dozen Poster",
-    url: "https://res.cloudinary.com/ddjxpkewq/image/upload/f_auto,q_auto/v1/dirtyDozen/ohrjjkyqzw79yl4mknyq",
-    fileType: "img",
-    category: "Brand",
-    categorySpecific: "an exercise in type hierarchy.",
+    id: "touchdesignerSeries",
+    name: "Touchdesigner Experiments",
+    url: "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/touchdesigner/tn1qnhqpbextyksfdpvk",
+
+    fileType: "mp4",
+    category: "Generative Design",
+    categorySpecific: "some generative procedural animations.",
   },
   // {
-  //   id: "15-112",
-  //   name: "Golden Contact,",
-  //   url: "/imgs/goldenContactSheetThumbnail.png",
-  //   category: "code",
-  //   categorySpecific: "a python prototyping tool crafted to the golden ratio.",
-  // },
-  // {
-  //   id: "allTimely",
-  //   name: "All Timely,",
-  //   url: "/imgs/102624_homePage.png",
-  //   category: "UI/UX",
-  //   categorySpecific: "AllTimely_Heroimg UI/UX.",
-
+  //   id: "typographyAnimation",
+  //   name: "Kinetic type animation",
+  //   url: "https://res.cloudinary.com/ddjxpkewq/image/upload/f_auto,q_auto/v1/c-animation/s3k9v38udigxjapryyxa",
+  //   fileType: "img",
+  //   category: "Motion",
+  //   categorySpecific: "a study in time and motion narrating a typeface.",
   // },
 ];
